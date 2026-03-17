827317e968f5e81e0e954742b0eb80a08f106c31
Son-JuHwan <sonjuhawn@gmail.com>
2026-03-18 00:37:05 +0900

    인벤토리 진행중

---
+++ ColorEquality/Color.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class Color : IEquatable<Color>
+{
+    public uint R { get; set; }
+    public uint G { get; set; }
+    public uint B { get; set; }
+
+    public Color(uint r, uint g, uint b)
+    {
+        R = r; G = g; B = b;
+    }
+
+    public bool Equals(Color other)
+    {
+        return other.R == R && other.G == G && other.B == B;
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj is Color)
+        {
+            return Equals(obj as Color);
+        }
+        return base.Equals(obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(R, G, B);
+    }
+
+    public bool IsSimilar(Color other, int threshold)
+    {
+        if (Math.Abs(other.R - R) < threshold && Math.Abs(other.G - G) < threshold && Math.Abs(other.B - B) < threshold)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"RGB({R}, {G}, {B})";
+    }
+}
--- FlexibleInventory/Program.cs
+++ FlexibleInventory/Program.cs
@@ -1,4 +1,56 @@
 using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 
 // README.md를 읽고 코드를 작성하세요.
 Console.WriteLine();
+
+List<Item> items = new()
+{
+    new Item("불꽃 검", "무기","전설"),
+    new Item("얼음 단검", "무기","희귀"),
+    new Item("철 갑옷", "방어구","일반"),
+    new Item("미스릴 방패", "방어구","희귀"),
+    new Item("체력 물약", "소비","일반"),
+};
+
+
+class Item
+{
+    public string Name { get; set; }
+    public string Type { get; set; }
+    public string Grade { get; set; }
+
+    public Item(string name, string type, string grade)
+    {
+        Name = name;
+        Type = type;
+        Grade = grade;
+    }
+}
+
+class ItemTypeComparer : EqualityComparer<Item>
+{
+    public override bool Equals(Item x, Item y)
+    {
+        return x.Type == y.Type;
+    }
+
+    public override int GetHashCode(Item obj)
+    {
+        return obj.Type.GetHashCode();
+    }
+}
+
+class ItemGradeComparer : EqualityComparer<Item>
+{
+    public override bool Equals(Item x, Item y)
+    {
+        return x.Grade == y.Grade;
+    }
+
+    public override int GetHashCode(Item obj)
+    {
+        return obj.Grade.GetHashCode();
+    }
+}
